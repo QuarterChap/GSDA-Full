a4f4476910f6550beb67829898eda3051c5a4e57
Julian Cearley <c3arl3y@julians-macbook-pro.local>
2018-10-07 22:30:30 -0400

    Good start on our Menu

--- GSDA/ViewControllers/MainMenuViewController.swift
+++ GSDA/ViewControllers/MainMenuViewController.swift
@@ -10,11 +10,31 @@ import UIKit
 
 class MainMenuViewController: UIViewController {
 
+    let logoImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "GSDALogo")
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFill
+        return imageView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationController?.isNavigationBarHidden = true
         view.backgroundColor = UIColor(r: 145, g: 183, b: 219)
         navigationItem.title = "MainMenuViewController"
         
+        view.addSubview(logoImageView)
+        
+        setupLogoImageView()
+        
     }
+    
+    func setupLogoImageView() {
+        logoImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: 0).isActive = true
+        logoImageView.topAnchor.constraint(equalTo: view.topAnchor, constant: 25).isActive = true
+        logoImageView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.4).isActive = true
+        logoImageView.heightAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.4).isActive = true
+    }
+    
 }
