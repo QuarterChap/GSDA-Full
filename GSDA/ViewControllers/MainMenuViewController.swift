95e5651a279035b9f99cd9e02ae1fba189c1eb0a
Christopher <christopherponce@hotmail.com>
2018-10-07 22:56:10 -0400

    created logoIV and its setup function

--- GSDA/ViewControllers/MainMenuViewController.swift
+++ GSDA/ViewControllers/MainMenuViewController.swift
@@ -9,13 +9,24 @@
 import UIKit
 
 class MainMenuViewController: UIViewController {
-
+    //julian
     let logoImageView: UIImageView = {
         let imageView = UIImageView()
         imageView.image = UIImage(named: "GSDALogo")
         imageView.translatesAutoresizingMaskIntoConstraints = false
         imageView.contentMode = .scaleAspectFill
         return imageView
+    }()
+    //chris
+    let logoIV: UIImageView = {
+        
+        let logoImageV = UIImageView()
+        
+        logoImageV.image = UIImage(named: "GSDALogo")
+        logoImageV.translatesAutoresizingMaskIntoConstraints = false
+        logoImageV.contentMode = .scaleAspectFill
+        
+        return logoImageV
     }()
     
     override func viewDidLoad() {
@@ -29,12 +40,22 @@ class MainMenuViewController: UIViewController {
         setupLogoImageView()
         
     }
-    
+    //julian
     func setupLogoImageView() {
         logoImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: 0).isActive = true
         logoImageView.topAnchor.constraint(equalTo: view.topAnchor, constant: 25).isActive = true
         logoImageView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.4).isActive = true
         logoImageView.heightAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.4).isActive = true
+    }
+    
+    
+    //chris
+    func setupLogoIV() {
+        
+        logoIV.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: 0).isActive = true
+        logoIV.topAnchor.constraint(equalTo: view.topAnchor, constant: 25).isActive = true
+        logoImageView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.5).isActive = true
+        logoImageView.heightAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.5).isActive = true
     }
     
 }
