b99fd547ffd114214847094fbf9dcfa28507dd3a
Julian Cearley <c3arl3y@julians-macbook-pro.local>
2018-10-07 21:53:16 -0400

    Preparing for the pro-grammatical way of doing things

---
+++ GSDA/ViewControllers/MainMenuViewController.swift
@@ -0,0 +1,20 @@
+//
+//  MainMenuViewController.swift
+//  GSDA
+//
+//  Created by Julian Cearley on 10/7/18.
+//  Copyright © 2018 Cearley-Programs. All rights reserved.
+//
+
+import UIKit
+
+class MainMenuViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationController?.isNavigationBarHidden = true
+        view.backgroundColor = UIColor(r: 145, g: 183, b: 219)
+        navigationItem.title = "MainMenuViewController"
+        
+    }
+}
