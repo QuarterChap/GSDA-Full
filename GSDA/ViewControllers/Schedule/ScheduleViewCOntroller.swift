2ca50219a626beddabe22d68d26709c5250168ad
joriahlasater <joriahlasaterdev@gmail.com>
2019-02-12 13:03:50 -0600

    Finished Schedule

--- GSDA/ViewControllers/Schedule/ScheduleViewCOntroller.swift
+++ GSDA/ViewControllers/Schedule/ScheduleViewCOntroller.swift
@@ -11,6 +11,8 @@ import JTAppleCalendar
 
 final class ScheduleViewController: UIViewController {
     
+    var currentDate: Date!
+    
     let monthLabel: UILabel = {
         let label = UILabel()
         label.text = "January"
@@ -31,11 +33,10 @@ final class ScheduleViewController: UIViewController {
         cView.calendarDelegate = self
         cView.calendarDataSource = self
         cView.scrollingMode = .stopAtEachCalendarFrame
-        
         return cView
     }()
     
-    let dayNames = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+    let dayNames = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
     var daysWithNotes = [Date]()
 
     var dayLabels: [UILabel] {
@@ -60,7 +61,6 @@ final class ScheduleViewController: UIViewController {
         return stackView
     }()
     
-    
     lazy var mainMenuButton: UIButton = {
         let button = UIButton(type:  .system)
         button.backgroundColor = UIColor(r: 166, g: 210, b: 253)
@@ -75,7 +75,73 @@ final class ScheduleViewController: UIViewController {
         return button
     }()
     
-    let formatter = DateFormatter()
+    let yearLabel: UILabel = {
+        let label = UILabel()
+        label.text = "2019"
+        label.textAlignment = .center
+        return label
+    }()
+    
+    lazy var leftButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("<---", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        button.addTarget(self, action: #selector(leftButtonPressed), for: .touchUpInside)
+        return button
+    }()
+    
+    lazy var rightButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("--->", for: .normal)
+        button.addTarget(self, action: #selector(rightButtonPressed), for: .touchUpInside)
+        button.setTitleColor(.black, for: .normal)
+        return button
+    }()
+    
+    @objc func leftButtonPressed() {
+        guard let date = Calendar.current.date(byAdding: .year, value: -1, to: currentDate) else {
+            return
+        }
+        if date < startDate {
+            return
+        }
+        
+        calenderView.scrollToDate(date)
+        updateYearLabel(date: date)
+        currentDate = date
+    }
+    
+    @objc func rightButtonPressed() {
+        guard let date = Calendar.current.date(byAdding: .year, value: 1, to: currentDate) else {
+            return
+        }
+        
+        if date > endDate {
+            return
+        }
+        calenderView.scrollToDate(date)
+        updateYearLabel(date: date)
+        currentDate = date
+    }
+    
+    lazy var yearStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [leftButton, yearLabel, rightButton])
+        stackView.axis = .horizontal
+        stackView.distribution = .fillEqually
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+    
+    let formatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy MM dd"
+        formatter.timeZone = Calendar.current.timeZone
+        formatter.locale = Calendar.current.locale
+        return formatter
+    }()
+    
+    lazy var startDate = formatter.date(from: "2019 01 01")!
+    lazy var endDate = formatter.date(from: "2025 12 31")!
 
     override func viewDidLoad() {
         setupSubViews()
@@ -116,6 +182,7 @@ private extension ScheduleViewController {
         view.addSubview(mainMenuButton)
         view.addSubview(monthLabel)
         view.addSubview(daysStackView)
+        view.addSubview(yearStackView)
         
         monthLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
         monthLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
@@ -130,7 +197,13 @@ private extension ScheduleViewController {
         calenderView.topAnchor.constraint(equalTo: daysStackView.bottomAnchor).isActive = true
         calenderView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
         calenderView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
-        calenderView.heightAnchor.constraint(equalToConstant: 400).isActive = true
+        calenderView.heightAnchor.constraint(equalToConstant: 300).isActive = true
+        
+        //Setup year
+        yearStackView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        yearStackView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        yearStackView.topAnchor.constraint(equalTo: calenderView.bottomAnchor, constant: 20).isActive = true
+        yearStackView.bottomAnchor.constraint(equalTo: mainMenuButton.bottomAnchor, constant: -50).isActive = true
         
         mainMenuButton.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
         mainMenuButton.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
@@ -140,12 +213,21 @@ private extension ScheduleViewController {
 }
 
 extension ScheduleViewController: JTAppleCalendarViewDelegate, JTAppleCalendarViewDataSource {
+    func handleCellTextColor(cell: DayCell?, cellState: CellState) {
+        if cellState.dateBelongsTo != .thisMonth {
+            cell?.dateLabel.textColor = .lightGray
+        } else {
+            cell?.dateLabel.textColor = .black
+        }
+    }
+    
     func calendar(_ calendar: JTAppleCalendarView, willDisplay cell: JTAppleCell, forItemAt date: Date, cellState: CellState, indexPath: IndexPath) {
         // Check documentation
     }
     
     func calendar(_ calendar: JTAppleCalendarView, cellForItemAt date: Date, cellState: CellState, indexPath: IndexPath) -> JTAppleCell {
         if let cell = calenderView.dequeueReusableJTAppleCell(withReuseIdentifier: "cell", for: indexPath) as? DayCell {
+            handleCellTextColor(cell: cell, cellState: cellState)
             cell.setup()
             cell.dateLabel.text = cellState.text
             
@@ -160,15 +242,7 @@ extension ScheduleViewController: JTAppleCalendarViewDelegate, JTAppleCalendarVi
     }
     
     func configureCalendar(_ calendar: JTAppleCalendarView) -> ConfigurationParameters {
-        formatter.dateFormat = "yyyy MM dd"
-        formatter.timeZone = Calendar.current.timeZone
-        formatter.locale = Calendar.current.locale
-        
-        
-        let startDate = formatter.date(from: "2017 01 01")!
-        let endDate = formatter.date(from: "2017 12 31")!
-        
-        let parameters = ConfigurationParameters(startDate: startDate, endDate: endDate)
+        let parameters = ConfigurationParameters(startDate: startDate, endDate: endDate, numberOfRows: 6, calendar: Calendar.current, generateInDates: .forAllMonths, generateOutDates: .tillEndOfRow, firstDayOfWeek: .sunday, hasStrictBoundaries: true)
         return parameters
     }
     
@@ -177,6 +251,7 @@ extension ScheduleViewController: JTAppleCalendarViewDelegate, JTAppleCalendarVi
             return
         }
         updateCalenderLabel(date: date)
+        updateYearLabel(date: date)
     }
     
     func calendar(_ calendar: JTAppleCalendarView, didSelectDate date: Date, cell: JTAppleCell?, cellState: CellState) {
@@ -191,8 +266,15 @@ extension ScheduleViewController: JTAppleCalendarViewDelegate, JTAppleCalendarVi
     }
     
     func updateCalenderLabel(date: Date) {
+        currentDate = date
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "MMMM"
         monthLabel.text = dateFormatter.string(from: date)
+    }
+    
+    func updateYearLabel(date: Date) {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "YYYY"
+        yearLabel.text = dateFormatter.string(from: date)
     }
 }
